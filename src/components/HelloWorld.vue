43231da74504341ce523006872a02c1d12e2d82a
giuseppedicosmo530@gmail.com <giuseppedicosmo530@gmail.com>
2022-09-30 12:07:24 +0200

    .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -7,6 +7,7 @@
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta error harum iusto expedita temporibus nobis ipsum inventore blanditiis similique, maxime voluptatum, sit consequatur possimus laudantium sequi illo quia repellendus minus.</p>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-pwa" target="_blank" rel="noopener">pwa</a></li>
